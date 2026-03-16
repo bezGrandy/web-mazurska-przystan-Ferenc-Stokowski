f659d4168548c5c2263242e3bc26955001de8755
bezGrandy <wojciech.stokowski@cyfroweszkoly.pl>
2026-03-17 00:02:50 +0100

    add amount display to rental time range input using usestate

--- src/components/ReservationCalculator.tsx
+++ src/components/ReservationCalculator.tsx
@@ -1,44 +1,53 @@
-import React from "react";
+import React, {useState} from "react";
 
 const ReservationCalculator: React.FC = () => {
+
+    const [hours, setHours] = useState<number>(1);
+
     return (
         <div className="app-container">
             <h1>Kalkulator wynajmu</h1>
 
-    <form className="reservation-form">
+        <form className="reservation-form">
 
-        {/* Imie */}
+            {/* Imie */}
 
-        <div className="form-element">
-            <label htmlFor="name">Imię</label>
-            <input id="name" type="text" placeholder="Twoje imię" />
-        </div>
+            <div className="form-element">
+                <label htmlFor="name">Imię</label>
+                <input id="name" type="text" placeholder="Twoje imię" />
+            </div>
 
-    {/* Sprzet */}
+            {/* Sprzet */}
 
-    <div className="form-element">
-        <label htmlFor="equipment">Wybierz sprzęt</label>
-        <select id="equipment">
-            <option value="kayak">Kajak (20zł/h)</option>
-            <option value="water-bike">Rower wodny (35zł/h)</option>
-            <option value="omega">Omega (150zł/h)</option>
-        </select>
-    </div>
+            <div className="form-element">
+                <label htmlFor="equipment">Wybierz sprzęt</label>
+                <select id="equipment">
+                    <option value="kayak">Kajak (20zł/h)</option>
+                    <option value="water-bike">Rower wodny (35zł/h)</option>
+                    <option value="omega">Omega (150zł/h)</option>
+                </select>
+            </div>
 
-    {/* Ostrzezenie o zaglowce */}
+            {/* Ostrzezenie o zaglowce */}
 
-    <div className="warning">
-          Przy żaglówce wymagany jest paten żeglarski!
-    </div>
+            <div className="warning">
+                  Przy żaglówce wymagany jest paten żeglarski!
+            </div>
 
-    {/* Godziny */}
+            {/* Godziny */}
 
-    <div className="form-element">
-    <label htmlFor="hours">Liczba godzin</label>
-    <input type="range" min="1" max="8" id="hours" step={1} />
-        </div>
+            <div className="form-element">
+                <label htmlFor="hours">
+                    Liczba godzin: {hours}
+                </label>
 
-    </form>
+                <input
+                    type="range" min="1" max="8" step={1} id="hours" value={hours}
+                    onChange={(e) => setHours(Number(e.target.value))}
+                />
+            </div>
+
+        </form>
     </div>
     );
 };
