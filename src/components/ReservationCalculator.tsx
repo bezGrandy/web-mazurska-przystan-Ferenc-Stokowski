18ac042ce65054c6b8b74e774d615635deb050e0
amotoreq <antoni.ferenc@cyfroweszkoly.pl>
2026-03-19 17:17:57 +0100

    Add useState to all form input and calculate logic to form

--- src/components/ReservationCalculator.tsx
+++ src/components/ReservationCalculator.tsx
@@ -1,9 +1,25 @@
 import React, {useState} from "react";
 import "./ReservationCalculator.css";
+import { calculatePrice } from "../Logic/CalculatorLogic";
+import type {EquipmentType} from "../Logic/CalculatorLogic";
 
 const ReservationCalculator: React.FC = () => {
 
     const [hours, setHours] = useState<number>(1);
+    const [name, setName] = useState("");
+    const [equipment, setEquipment] = useState<EquipmentType>("kayak");
+    const [childVest, setChildVest] = useState(false);
+    const [instructor, setInstructor] = useState(false);
+    const [payment, setPayment] = useState("");
+    const [acceptedRules, setAcceptedRules] = useState(false);
+
+    const totalPrice = calculatePrice({
+        equipment,
+        hours,
+        childVest,
+        instructor
+    });
+
 
     return (
         <div className="app-container">
@@ -15,14 +31,18 @@ const ReservationCalculator: React.FC = () => {
 
             <div className="form-element">
                 <label htmlFor="name">Imię</label>
-                <input id="name" type="text" placeholder="Twoje imię" />
+                <input id="name" type="text" placeholder="Twoje imię"
+                       value={name}
+                       onChange={(e) => setName(e.target.value)} />
             </div>
 
             {/* Sprzet */}
 
             <div className="form-element">
                 <label htmlFor="equipment">Wybierz sprzęt</label>
-                <select id="equipment">
+                <select id="equipment"
+                        value={equipment}
+                        onChange={(e) => setEquipment(e.target.value as EquipmentType)}>
                     <option value="kayak">Kajak (20zł/h)</option>
                     <option value="water-bike">Rower wodny (35zł/h)</option>
                     <option value="omega">Omega (150zł/h)</option>
@@ -31,9 +51,13 @@ const ReservationCalculator: React.FC = () => {
 
             {/* Ostrzezenie o zaglowce */}
 
-            <div className="warning"> {/* zmien tutaj ze pokazuje sie tylko przy wyborze omegi */}
-                  Przy żaglówce wymagany jest paten żeglarski!
-            </div>
+
+            {equipment === "omega" && (
+                <div className="warning">
+                    Przy żaglówce wymagany jest patent żeglarski!
+                </div>
+            )}
+
 
             {/* Godziny */}
 
@@ -51,14 +75,18 @@ const ReservationCalculator: React.FC = () => {
             {/* dodatkowe */}
             <div className="form-element">
                 <label>
-                    <input type="checkbox" />
+                    <input type="checkbox"
+                           checked={childVest}
+                           onChange={(e) => setChildVest(e.target.checked)}/>
                     Kapok dla dziecka (+5zł)
                 </label>
             </div>
 
             <div className="form-element">
                 <label>
-                    <input type="checkbox" />
+                    <input type="checkbox"
+                           checked={instructor}
+                           onChange={(e) => setInstructor(e.target.checked)} />
                     Instruktor (+50zł/h)
                 </label>
             </div>
@@ -69,12 +97,16 @@ const ReservationCalculator: React.FC = () => {
                 <label>Metoda płatności</label>
 
                 <label>
-                    <input type="radio" name="payment" />
+                    <input type="radio" name="payment"
+                           value="card"
+                           onChange={(e) => setPayment(e.target.value)} />
                     Karta
                 </label>
 
                 <label>
-                    <input type="radio" name="payment" />
+                    <input type="radio" name="payment"
+                           value="blik"
+                           onChange={(e) => setPayment(e.target.value)} />
                     BLIK
                 </label>
             </div>
@@ -82,7 +114,9 @@ const ReservationCalculator: React.FC = () => {
             {/* Regulamin */}
             <div className="form-element">
                 <label>
-                    <input type="checkbox" />
+                    <input type="checkbox"
+                           checked={acceptedRules}
+                           onChange={(e) => setAcceptedRules(e.target.checked)} />
                     Akceptuję regulamin
                 </label>
             </div>
@@ -90,10 +124,12 @@ const ReservationCalculator: React.FC = () => {
             {/* Cena */}
 
             <div className="price-box">
-                Cena: <strong>0 zł</strong>
+                Cena: <strong>{totalPrice} zł</strong>
             </div>
 
-            <button className="submit-button">
+            <button className="submit-button"
+                    disabled={!name || !payment || !acceptedRules}
+            >
                 Rezerwuj
             </button>
 
